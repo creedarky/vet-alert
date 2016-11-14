b3fc282c468e5081ca28087edcba962dbd0b6389
Maximiliano Castro <max@axiomzen.co>
2016-11-14 17:20:01 -0300

    remove shrinkray

--- server/config/express.js
+++ server/config/express.js
@@ -7,7 +7,6 @@
 import express from 'express';
 import favicon from 'serve-favicon';
 import morgan from 'morgan';
-import shrinkRay from 'shrink-ray';
 import bodyParser from 'body-parser';
 import methodOverride from 'method-override';
 import cookieParser from 'cookie-parser';
